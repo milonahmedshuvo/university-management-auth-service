171757b5465db9f06cc569d173227bf5110bb3e8
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-05-31 10:15:34 +0600

    express mongoose corse parse

---
+++ src/App.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express'
+import cors from "cors"
+const app:Application = express()
+
+// cors using 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+
+
+// testing parpus 
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World!')
+})
+
+
+
+export default app
