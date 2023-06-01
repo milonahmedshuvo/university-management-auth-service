f47f2aa1e9862b3fd9e469b73310f2ddc22ac684
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-06-01 09:55:56 +0600

    both check

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 import app from './App'
-import config from "./Config/index"
-
+import config from './Config/index'
 
 async function main() {
-    
-    try{
-        await mongoose.connect(config.database_url as string);
-        console.log("Database Connected success")
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database Connected success')
 
-
-
-        app.listen(config.port, () => {
-            console.log(` Application app listening on port ${config.port}`)
-          })
-
-    }catch(err){
-        console.log("Database Connect Filed", err)
-    }
-  
-    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    app.listen(config.port, () => {
+      console.log(` Application app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Database Connect Filed', err)
   }
 
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
 
-  main()
-
-  
+main()
